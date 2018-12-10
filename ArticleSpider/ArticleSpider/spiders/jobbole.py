a6419112bac49e16adc240fb8b436c9a945266da
futhead <futhead@163.com>
2018-12-10 23:31:56 +0800

    【A】初始化ArticleSpider

---
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import Request
+from urllib import parse
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ["http://blog.jobbole.com/all-posts"]
+
+    def parse(self, response):
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url"},
+                          callback=self.parse_content)
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first()
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_content(self):
+        pass
